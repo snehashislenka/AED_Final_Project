bab994a49a0edc1fea4788a2702d45f456fd6c58
snehashislenka <lenka.s@northeastern.edu>
2022-12-02 22:05:47 -0500

    Integrated java mail jar for email verification and utils for MySQL

---
+++ AED_Final_Project/src/email/util/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package email.util;
+
+/**
+ *
+ * @author slenk
+ */
+public class Test {
+    public static void main(String[] args) throws Exception{
+        EmailUtil.sendEmail("slenkadev@gmail.com");
+    }
+}
---
+++ AED_Final_Project/src/model/Person/Roles.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.Person;
+
+/**
+ *
+ * @author slenk
+ */
+public enum Roles {
+    PASSENGER,
+    SYSTEM_ADMIN,
+    FLIGHT_ADMIN,
+    TRAVEL_ADMIN
+}
